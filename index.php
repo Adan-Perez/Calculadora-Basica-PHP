db7d844d501e7b37a6bc952edbf5f0c32f90550d
JesusAdan <jesusadanperezrodriguez@alumno.ieselrincon.es>
2023-05-04 18:11:15 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calculadora</title>
+</head>
+<body>
+	<form method="post">
+		<label>Primer número:</label><br>
+		<input type="number" name="num1"><br><br>
+		<label>Segundo número:</label><br>
+		<input type="number" name="num2"><br><br>
+		<label>Operación:</label><br>
+		<select name="operacion">
+			<option value="addition">Suma</option>
+			<option value="substraction">Resta</option>
+			<option value="multiplication">Multiplicación</option>
+			<option value="division">División</option>
+		</select>
+        <br><br>
+		<input type="submit" value="Calcular">
+	</form>
+
+	<?php
+	if($_POST){
+		$num1 = $_POST['num1'];
+		$num2 = $_POST['num2'];
+		$operacion = $_POST['operacion'];
+		switch($operacion){
+			case "addition":
+				echo "El resultado es: " . ($num1 + $num2);
+				break;
+			case "substraction":
+				echo "El resultado es: " . ($num1 - $num2);
+				break;
+			case "multiplication":
+				echo "El resultado es: " . ($num1 * $num2);
+				break;
+			case "division":
+				if($num2 == 0){
+					echo "¡Error! No se puede dividir entre cero";
+				}else{
+					echo "El resultado es: " . ($num1 / $num2);
+				}
+				break;
+			default:
+				echo "Operación no válida, por favor intente de nuevo";
+				break;
+		}
+	}
+	?>
+</body>
+</html>
